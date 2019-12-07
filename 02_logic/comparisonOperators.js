5fe6bfb4b2ab921fddefe24cc2d0b19d8a691d52
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2019-12-07 17:58:46 -0500

    start if logic

---
+++ 02_logic/comparisonOperators.js
@@ -0,0 +1,11 @@
+/*
+Comparisons:
+  > greater than
+  < less than
+  >= greater than or equal to
+  <= less than or equal to
+  == equality
+  != not equal
+  === strict equality
+  !== strict non-equality
+*/
