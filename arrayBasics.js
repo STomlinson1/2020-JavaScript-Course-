8eaa70f6b9ecccb8256d16eda05a2efcc0e25b69
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2019-12-07 20:28:42 -0500

    Start array lesson

---
+++ arrayBasics.js
@@ -0,0 +1,28 @@
+/* 
+Goals of section:
+  work with arrays
+  write object literals
+  understand reference types
+  use common array/oject methods
+
+Arrays:
+  Ordered collection of values
+
+  */
+
+// To make an empty array
+let students = [];
+
+// An array of strings
+let colors = ['red','orange','green','purple'];
+
+// An array of numbers
+let lottoNums = [18,22,34,64,26,11];
+
+// A mixed array
+let stuff = [true, 68, 'car', null];
+
+// Example 1
+let shoppingList = ['cheese','cereal','ice'];
+console.log(shoppingList);
+console.log(shoppingList[1]);
