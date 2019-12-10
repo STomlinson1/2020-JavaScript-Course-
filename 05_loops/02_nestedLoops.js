a3ca66d2ae259a9cd9a7c6b4755c3b91446c6532
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2019-12-10 15:52:00 -0500

    working on loops

--- 05_loops/02_nestedLoops.js
+++ 05_loops/02_nestedLoops.js
@@ -4,3 +4,13 @@ for (let i = 1; i <= 10; i++) {
 		console.log(' Inner Loop', j);
 	}
 }
+
+const gameBoard = [ [ 4, 32, 8, 4 ], [ 64, 8, 32, 2 ], [ 8, 32, 16, 4 ], [ 2, 8, 4, 2 ] ];
+
+let totalScore = 0;
+for (let i = 0; i < gameBoard.length; i++) {
+	for (let j = 0; j < gameBoard[i].length; j++) {
+		totalScore += gameBoard[i][j];
+	}
+}
+console.log(totalScore);
