6ff9980087681a64982bb917c42b58d627c19ada
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2019-12-21 12:59:57 -0500

    p

---
+++ 11_TheDom/01_intro.js
@@ -0,0 +1,5 @@
+/* 
+The document object model 'The DOM' is a javascript representation of a webpage
+It's your JS 'window' into the contents of a webpage
+It's a bunch of objects that you can interact with via JS
+*/
