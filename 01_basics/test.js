ef7ba695a161ecc1d0ef2bc7d756dc52982ca472
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-01-01 23:47:17 -0500

    m

---
+++ 01_basics/test.js
@@ -0,0 +1,4 @@
+let player1 = 'Diana';
+let player2 = 'Stephon';
+
+console.log('lets play rock,paper,scissor');
---
+++ 12_ScoresProject/scores.js
@@ -0,0 +1,103 @@
+const warriorsGames = [
+	{
+		awayTeam : {
+			team     : 'Golden State',
+			points   : 119,
+			isWinner : true
+		},
+		homeTeam : {
+			team     : 'Houston',
+			points   : 106,
+			isWinner : false
+		}
+	},
+	{
+		awayTeam : {
+			team     : 'Golden State',
+			points   : 105,
+			isWinner : false
+		},
+		homeTeam : {
+			team     : 'Houston',
+			points   : 127,
+			isWinner : true
+		}
+	},
+	{
+		homeTeam : {
+			team     : 'Golden State',
+			points   : 126,
+			isWinner : true
+		},
+		awayTeam : {
+			team     : 'Houston',
+			points   : 85,
+			isWinner : false
+		}
+	},
+	{
+		homeTeam : {
+			team     : 'Golden State',
+			points   : 92,
+			isWinner : false
+		},
+		awayTeam : {
+			team     : 'Houston',
+			points   : 95,
+			isWinner : true
+		}
+	},
+	{
+		awayTeam : {
+			team     : 'Golden State',
+			points   : 94,
+			isWinner : false
+		},
+		homeTeam : {
+			team     : 'Houston',
+			points   : 98,
+			isWinner : true
+		}
+	},
+	{
+		homeTeam : {
+			team     : 'Golden State',
+			points   : 115,
+			isWinner : true
+		},
+		awayTeam : {
+			team     : 'Houston',
+			points   : 86,
+			isWinner : false
+		}
+	},
+	{
+		awayTeam : {
+			team     : 'Golden State',
+			points   : 101,
+			isWinner : true
+		},
+		homeTeam : {
+			team     : 'Houston',
+			points   : 92,
+			isWinner : false
+		}
+	}
+];
+
+const ulParent = document.createElement('ul');
+for (let game of warriorsGames) {
+	//destructuring each game object
+	const { homeTeam, awayTeam } = game;
+	const gameLi = document.createElement('li');
+	//destructure away and home team
+	const { team: hTeam, points: hPoints } = homeTeam;
+	const { team: aTeam, points: aPoints } = awayTeam;
+	const teamNames = `${aTeam} @ ${hTeam}`;
+	const scoreLine = `${aPoints}--${hPoints}`;
+
+	gameLi.innerText = `${teamNames} ${scoreLine}`;
+	ulParent.appendChild(gameLi);
+}
+
+document.body.prepend(ulParent);
