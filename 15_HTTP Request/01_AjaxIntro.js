b295ad1e19d77010b9d8f21bc08152d6d47a6fcf
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-01-13 18:13:34 -0500

    start HTTP Request section

---
+++ 15_HTTP Request/01_AjaxIntro.js
@@ -0,0 +1,5 @@
+/* 
+AJAX: Asynchronous JavaScript And XML/JSON
+
+The idea of AJAX is using request we can make via javaScript Asynchronously to load or send data from a server. We communicate with the server behind the scenes instead of reloading the entire page. The request is sent based off a keyboard event.  The response is in JSON format. JSON is a format for sending data.
+*/
