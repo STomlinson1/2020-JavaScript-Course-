cbce7f1934862296916091af9a4d31cf69425590
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-01-15 13:13:41 -0500

    working with fetch and promises

--- 15_HTTP Request/02_fetch.js
+++ 15_HTTP Request/02_fetch.js
@@ -41,4 +41,64 @@ const promExample = fetch('https://jsonplaceholder.typicode.com/users')
 		console.log(error);
 	});
 
-const prom1 = fetch('https://swapi.co/api/planets/');
+// Fetch Chaining
+const prom1 = fetch('https://swapi.co/api/planets/')
+	.then((response) => {
+		if (response.ok) {
+			return response.json();
+		} else {
+			throw new Error(`Status Code Error: ${response.status}`);
+		}
+	})
+	.then((data) => {
+		for (let planet of data.results) {
+			// console.log(planet);
+		}
+		const filmURL = data.results[0].films[0];
+		return fetch(filmURL);
+	})
+	.then((response) => {
+		if (response.ok) {
+			return response.json();
+		} else {
+			throw new Error(`Status Code Error: ${response.status}`);
+		}
+	})
+	.then((data) => {
+		// console.log('Fetched First Film, based off first planet');
+		// console.log(data.title);
+	})
+	.catch((response) => {
+		console.log(response);
+	});
+
+// Fetch Chaining Refactor
+const checkStatusAndParse = (response) => {
+	if (response.ok) {
+		return response.json();
+	} else {
+		throw new Error(`Status Code Error: ${response.status}`);
+	}
+};
+
+const printPlanets = (data) => {
+	console.log('Loaded 10 more planets');
+	for (let planet of data.results) {
+		console.log(planet.name);
+	}
+	return Promise.resolve(data.next);
+};
+
+const fetchMorePlanets = (url) => {
+	return fetch(url);
+};
+
+fetch('https://swapi.co/api/planets/')
+	.then(checkStatusAndParse)
+	.then(printPlanets)
+	.then(fetchMorePlanets)
+	.then(checkStatusAndParse)
+	.then(printPlanets)
+	.catch((response) => {
+		console.log(response);
+	});
