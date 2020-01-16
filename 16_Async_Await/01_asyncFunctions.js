f1b43dbca247ee006308f8b146508d37bde5492b
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-01-16 00:17:47 -0500

    multiple awaits

--- 16_Async_Await/01_asyncFunctions.js
+++ 16_Async_Await/01_asyncFunctions.js
@@ -48,10 +48,60 @@ async function getPlanets() {
 		console.log('In Catch', e);
 	}
 }
-
 // getPlanets().catch((error) => {
 // 	console.log('In Catch');
 // 	console.log(error);
 // });
-
 getPlanets();
+
+// Refactor from callback hell lesson
+const moveX = (element, amount, delay) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const bodyBoundary = document.body.clientWidth;
+			const elRight = element.getBoundingClientRect().right;
+			const currLeft = element.getBoundingClientRect().left;
+			if (elRight + amount > bodyBoundary) {
+				reject({ bodyBoundary, elRight, amount });
+			} else {
+				element.style.transform = `translateX(${currLeft + amount}px)`;
+				resolve();
+			}
+		}, delay);
+	});
+};
+
+const btn = document.querySelector('button');
+async function animateRight(el) {
+	await moveX(el, 150, 1000);
+	await moveX(el, 150, 1000);
+	await moveX(el, 150, 1000);
+	await moveX(el, 150, 1000);
+	await moveX(el, 150, 1000);
+	await moveX(el, 150, 1000);
+}
+
+animateRight(btn).catch((error) => console.log('All Done', error));
+
+// moveX(btn, 150, 1000)
+// 	.then(() => moveX(btn, 150, 1000))
+// 	.then(() => moveX(btn, 150, 1000))
+// 	.then(() => moveX(btn, 150, 1000))
+// 	.then(() => moveX(btn, 150, 1000))
+// 	.then(() => moveX(btn, 150, 1000))
+// 	.then(() => moveX(btn, 150, 1000))
+// 	.then(() => moveX(btn, 150, 1000))
+// 	.catch(({ bodyBoundary, amount, elRight }) => {
+// 		console.log(`Body is ${bodyBoundary}px wide`);
+// 		console.log(`Element is at ${elRight}px, ${amount}px is too large`);
+// 	});
+
+async function get3Pokemon() {
+	const poke1 = await axios.get('https://pokeapi.co/api/v2/pokemon/1');
+	const poke2 = await axios.get('https://pokeapi.co/api/v2/pokemon/1');
+	const poke3 = await axios.get('https://pokeapi.co/api/v2/pokemon/1');
+
+	console.log(poke1.data);
+}
+
+get3Pokemon();
